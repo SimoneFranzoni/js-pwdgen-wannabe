c2ac251b71492008723b0b9e66b29c421a516057
SimoneFranzoni <simofranzoni99@gmail.com>
2021-10-20 16:51:19 +0200

    last-commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 const firstname = prompt("Inserisci il nome");
 const lastname = prompt("Inserisci il cognome");
 const favouritecolor = prompt("Inserisci il tuo colore preferito");
+const number = 21;
 
-const password = firstname + lastname + favouritecolor;
+const password = firstname + lastname + favouritecolor +number;
 
 console.log(firstname);
 console.log(lastname);
 console.log(favouritecolor);
 console.log(password);
 
-const textToPrint =
-`
-La tua password è: ${password}
-`;
+const textToPrint = `La tua password è: ${password}`;
 
 console.log(textToPrint);
 
